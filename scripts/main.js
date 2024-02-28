e2475c18103393dc6df2a0a28581c5448c61807f
SammriddhGupta <sammriddhgupta@gmail.com>
2024-02-29 02:11:43 +1100

    feat: handle form with ajax and XMLHttpRequest

--- scripts/main.js
+++ scripts/main.js
@@ -1,97 +1,80 @@
-const currentUserToken={
-    currentUserId:"",
-    idToken:"",
-    role:""
-}
-function pageLoad()
-{
-cognitoApp.auth.parseCognitoWebResponse(window.location.href);
-        var currentUser =cognitoApp.auth.getCurrentUser();
+const currentUserToken = {
+currentUserId: "",
+idToken: "",
+role: "",
+};
+function pageLoad() {
+    cognitoApp.auth.parseCognitoWebResponse(window.location.href);
+    var currentUser = cognitoApp.auth.getCurrentUser();
 
-        if (currentUser) {
+    if (currentUser) {
+        cognitoApp.auth.getSession();
 
-            cognitoApp.auth.getSession();
+        currentSession = cognitoApp.auth.signInUserSession;
 
-            currentSession = cognitoApp.auth.signInUserSession;
+        currentUserToken.currentUserId = currentUser;
+        currentUserToken.idToken = currentSession.idToken.jwtToken;
+        console.info(currentUserToken);
 
-            currentUserToken.currentUserId = currentUser;
-            currentUserToken.idToken = currentSession.idToken.jwtToken;
-            console.info(currentUserToken);
-
-            var tokenDetails = parseJwt(currentSession.idToken.jwtToken)
-            if (tokenDetails['cognito:groups'])
-            {
-                var groups = tokenDetails['cognito:groups'][0];
-                currentUserToken.role = groups;
-            }
-            
+        var tokenDetails = parseJwt(currentSession.idToken.jwtToken);
+        if (tokenDetails["cognito:groups"]) {
+        var groups = tokenDetails["cognito:groups"][0];
+        currentUserToken.role = groups;
         }
-
-        $("#btnSignIn").on('click', function(btn){
-            cognitoApp.auth.getSession();
-        });
-
-        $("#btnSignOut").on('click', function(btn){
-            currentUserToken.role ="";
-            currentUserToken.idToken="";
-            currentUserToken.currentUserId="";
-            cognitoApp.auth.signOut();
-        });
-
-        $("#btnSignOut").hide();
-        $("#btnSignIn").hide();
-
-        if (currentUserToken.currentUserId==="")
-        {
-            $("#btnSignIn").show();
-        }
-        else
-        {
-            $("#btnSignOut").show();
-        }
-
-}
-
-
-
-    function parseJwt (token) {
-        var base64Url = token.split('.')[1];
-        var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-        var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
-            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-        }).join(''));
-
-        return JSON.parse(jsonPayload);
     }
 
-function setAuthHeader()
-{
+    $("#btnSignIn").on("click", function (btn) {
+        cognitoApp.auth.getSession();
+    });
 
-var forms= $('form');
-forms.submit(function(event) {
+    $("#btnSignOut").on("click", function (btn) {
+        currentUserToken.role = "";
+        currentUserToken.idToken = "";
+        currentUserToken.currentUserId = "";
+        cognitoApp.auth.signOut();
+    });
 
-    // Stop the form from submitting normally
-    event.preventDefault();
+    $("#btnSignOut").hide();
+    $("#btnSignIn").hide();
 
-    // Get form data
-    var formData = new FormData($(this)[0]);
-    console.log(formData);        
-    // Set up the AJAX request
-    $.ajax({
+    if (currentUserToken.currentUserId === "") {
+        $("#btnSignIn").show();
+    } else {
+        $("#btnSignOut").show();
+    }
+}
 
-      url: $(this).attr('action'),
-      type: 'POST',
-      beforeSend: function(request) {
-        request.setRequestHeader('Authorization', 'Bearer '+ currentUserToken.idToken);
-        //request.setRequestHeader('Content-type', 'multipart/form-data; boundary=---------------------------' + new Date().getTime());
-      },
-      data: formData,
-      contentType:false,
-      processData: false,
-      success: function(response) {
-     
-        window.location.href='/hotel/admin.html';
-      }
-    });    
-  });
-}        
+function parseJwt(token) {
+    var base64Url = token.split(".")[1];
+    var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+    var jsonPayload = decodeURIComponent(
+        window
+        .atob(base64)
+        .split("")
+        .map(function (c) {
+            return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+        })
+        .join("")
+    );
+
+    return JSON.parse(jsonPayload);
+}
+
+function setAuthHeader() {
+    form = $("form");
+    forms.submit(function (event) {
+        event.preventDefault(); // Stop the form from submitting normally
+
+        const xhr = new XMLHttpRequest();
+
+        xhr.open("POST", form.attr("action"));
+        xhr.setRequestHeader("Authorization", "Bearer " + currentUserToken.idToken);
+        xhr.setRequestHeader("Content-Type", "multipart/form-data");
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                console.log(xhr.responseText);
+            }
+        };
+        xhr.send(form.serialize()); // sending with ajax
+    });
+}
